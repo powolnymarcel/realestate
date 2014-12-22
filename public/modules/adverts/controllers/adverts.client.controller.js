129a1d58f2125f9d4ea3a4eb54898de49606dcc9
Yaser BOUHAFS <yaser.bouhafs@gmail.com>
2014-12-22 06:07:11 -0800

    zoom module implmentation

--- public/modules/adverts/controllers/adverts.client.controller.js
+++ public/modules/adverts/controllers/adverts.client.controller.js
@@ -15,6 +15,46 @@ angular.module('adverts').controller('AdvertsController', ['$scope', '$statePara
 								 visible:false};
 		};
 
+		$scope.zoom = function () {
+
+			$(document).ready(function() {
+				
+				$('#imageFullScreen').smartZoom({'containerClass':'zoomableContainer'});
+				
+				$('#topPositionMap,#leftPositionMap,#rightPositionMap,#bottomPositionMap').bind("click", moveButtonClickHandler);
+  				$('#zoomInButton,#zoomOutButton').bind("click", zoomButtonClickHandler);
+				
+				function zoomButtonClickHandler(e){
+			    	var scaleToAdd = 0.8;
+					if(e.target.id == 'zoomOutButton')
+						scaleToAdd = -scaleToAdd;
+					$('#imageFullScreen').smartZoom('zoom', scaleToAdd);
+			    }
+			    
+			    function moveButtonClickHandler(e){
+			    	var pixelsToMoveOnX = 0;
+					var pixelsToMoveOnY = 0;
+			
+					switch(e.target.id){
+						case "leftPositionMap":
+							pixelsToMoveOnX = 50;	
+						break;
+						case "rightPositionMap":
+							pixelsToMoveOnX = -50;
+						break;
+						case "topPositionMap":
+							pixelsToMoveOnY = 50;	
+						break;
+						case "bottomPositionMap":
+							pixelsToMoveOnY = -50;	
+						break;
+					}
+					$('#imageFullScreen').smartZoom('pan', pixelsToMoveOnX, pixelsToMoveOnY);
+			    }
+				
+			});
+		}
+
 		// Create new Advert
 		$scope.create = function() {
 			// Create new Advert object
