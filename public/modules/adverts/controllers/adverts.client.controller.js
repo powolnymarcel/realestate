eeb2edcea9dbe7ea2803c9822c12f5f6bdafcb52
Yaser BOUHAFS <yaser.bouhafs@gmail.com>
2014-12-24 11:54:41 -0800

    Zoom module

--- public/modules/adverts/controllers/adverts.client.controller.js
+++ public/modules/adverts/controllers/adverts.client.controller.js
@@ -15,47 +15,49 @@ angular.module('adverts').controller('AdvertsController', ['$scope', '$statePara
 								 visible:false};
 		};
 
-/*
+
 		$scope.zoom = function () {
 
 			$(document).ready(function() {
-				
-				$('#imageFullScreen').smartZoom({'containerClass':'zoomableContainer'});
-				
-				$('#topPositionMap,#leftPositionMap,#rightPositionMap,#bottomPositionMap').bind("click", moveButtonClickHandler);
-  				$('#zoomInButton,#zoomOutButton').bind("click", zoomButtonClickHandler);
-				
-				function zoomButtonClickHandler(e){
-			    	var scaleToAdd = 0.8;
-					if(e.target.id == 'zoomOutButton')
-						scaleToAdd = -scaleToAdd;
-					$('#imageFullScreen').smartZoom('zoom', scaleToAdd);
-			    }
-			    
-			    function moveButtonClickHandler(e){
+
+				function moveButtonClickHandler(e){
 			    	var pixelsToMoveOnX = 0;
 					var pixelsToMoveOnY = 0;
 			
 					switch(e.target.id){
-						case "leftPositionMap":
+						case 'leftPositionMap':
 							pixelsToMoveOnX = 50;	
 						break;
-						case "rightPositionMap":
+						case 'rightPositionMap':
 							pixelsToMoveOnX = -50;
 						break;
-						case "topPositionMap":
+						case 'topPositionMap':
 							pixelsToMoveOnY = 50;	
 						break;
-						case "bottomPositionMap":
+						case 'bottomPositionMap':
 							pixelsToMoveOnY = -50;	
 						break;
 					}
-					$('#imageFullScreen').smartZoom('pan', pixelsToMoveOnX, pixelsToMoveOnY);
+					$('#imgContainer').smartZoom('pan', pixelsToMoveOnX, pixelsToMoveOnY);
+			    }
+
+			    function zoomButtonClickHandler(e){
+			    	var scaleToAdd = 0.8;
+					if(e.target.id === 'zoomOutButton')
+						scaleToAdd = -scaleToAdd;
+					$('#imgContainer').smartZoom('zoom', scaleToAdd);
 			    }
 				
+				$('#imgContainer').smartZoom({'containerClass':'zoomableContainer'});
+				
+				$('#topPositionMap,#leftPositionMap,#rightPositionMap,#bottomPositionMap').bind('click', moveButtonClickHandler);
+  				$('#zoomInButton,#zoomOutButton').bind('click', zoomButtonClickHandler);
+				
+				
+				
 			});
-		}
-*/
+		};
+
 
 		// Create new Advert
 		$scope.create = function() {
--- public/modules/adverts/directives/adverts.client.directive.js
+++ public/modules/adverts/directives/adverts.client.directive.js
@@ -5,7 +5,8 @@ angular.module('adverts')
 	function ($http,$location,shapes,d3,Adverts,AdvertsPID,$,includeService){
 	
 	function drawShapes(scope,element) 
-	{			
+	{
+		
 
 		$http.get('/modules/adverts/data/data.json').success(function(data) 
 		{
@@ -94,6 +95,44 @@ angular.module('adverts')
 			        });
 		});
 	}
+/*
+			$(document).ready(function() {
+				
+				$('#imageFullScreen').smartZoom({'containerClass':'zoomableContainer'});
+				
+				$('#topPositionMap,#leftPositionMap,#rightPositionMap,#bottomPositionMap').bind("click", moveButtonClickHandler);
+  				$('#zoomInButton,#zoomOutButton').bind("click", zoomButtonClickHandler);
+				
+				function zoomButtonClickHandler(e){
+			    	var scaleToAdd = 0.8;
+					if(e.target.id == 'zoomOutButton')
+						scaleToAdd = -scaleToAdd;
+					$('#imageFullScreen').smartZoom('zoom', scaleToAdd);
+			    }
+			    
+			    function moveButtonClickHandler(e){
+			    	var pixelsToMoveOnX = 0;
+					var pixelsToMoveOnY = 0;
+			
+					switch(e.target.id){
+						case 'leftPositionMap':
+							pixelsToMoveOnX = 50;	
+						break;
+						case 'rightPositionMap':
+							pixelsToMoveOnX = -50;
+						break;
+						case 'topPositionMap':
+							pixelsToMoveOnY = 50;	
+						break;
+						case 'bottomPositionMap':
+							pixelsToMoveOnY = -50;	
+						break;
+					}
+					$('#imageFullScreen').smartZoom('pan', pixelsToMoveOnX, pixelsToMoveOnY);
+			    }
+				
+			});
+*/
 
 	//To disable by defaut all form elements inside 'form'
     $('#form :input').prop('disabled', true);
@@ -104,6 +143,7 @@ angular.module('adverts')
 	$('input[type=submit]').removeAttr('disabled');
 
 	});
+
 
 
 	return {
