71eadebeec5391f58a13ecc45451c301dc83024c
Yaser BOUHAFS <yaser.bouhafs@gmail.com>
2014-12-26 12:50:42 -0800

    désactivation du formulaire après avoir choisi une parcelle qui contient une annonce

--- public/modules/adverts/directives/adverts.client.directive.js
+++ public/modules/adverts/directives/adverts.client.directive.js
@@ -52,6 +52,7 @@ angular.module('adverts')
 						.success(function (advert) {
 							scope.findAdvertByPID(advert);
 							scope.showAdverts.visible = true;
+							$('#form :input').prop('disabled', true);
 							includeService.includeAdvert(advert);
 						}).error(function (err) {//If this pid is not yet use for an another advert
 							scope.showAdverts.visible = false;
@@ -95,44 +96,6 @@ angular.module('adverts')
 			        });
 		});
 	}
-/*
-			$(document).ready(function() {
-				
-				$('#imageFullScreen').smartZoom({'containerClass':'zoomableContainer'});
-				
-				$('#topPositionMap,#leftPositionMap,#rightPositionMap,#bottomPositionMap').bind("click", moveButtonClickHandler);
-  				$('#zoomInButton,#zoomOutButton').bind("click", zoomButtonClickHandler);
-				
-				function zoomButtonClickHandler(e){
-			    	var scaleToAdd = 0.8;
-					if(e.target.id == 'zoomOutButton')
-						scaleToAdd = -scaleToAdd;
-					$('#imageFullScreen').smartZoom('zoom', scaleToAdd);
-			    }
-			    
-			    function moveButtonClickHandler(e){
-			    	var pixelsToMoveOnX = 0;
-					var pixelsToMoveOnY = 0;
-			
-					switch(e.target.id){
-						case 'leftPositionMap':
-							pixelsToMoveOnX = 50;	
-						break;
-						case 'rightPositionMap':
-							pixelsToMoveOnX = -50;
-						break;
-						case 'topPositionMap':
-							pixelsToMoveOnY = 50;	
-						break;
-						case 'bottomPositionMap':
-							pixelsToMoveOnY = -50;	
-						break;
-					}
-					$('#imageFullScreen').smartZoom('pan', pixelsToMoveOnX, pixelsToMoveOnY);
-			    }
-				
-			});
-*/
 
 	//To disable by defaut all form elements inside 'form'
     $('#form :input').prop('disabled', true);
